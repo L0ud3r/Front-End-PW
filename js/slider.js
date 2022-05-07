44a1346b6dc6e2e7d9ea8ad594a36158fdf64139
Pedro Simões <69718437+L0ud3r@users.noreply.github.com>
2022-05-07 12:39:35 +0100

    Slider update

---
+++ js/slider.js
@@ -0,0 +1,47 @@
+const text1 = "Deve lavar o seu cão, pelo menos, uma vez por mês.";
+const text2 = "Deve dar de comer ao seu cão duas vezes por dia.";
+const text3 = "Deve levar o seu cão à rua, pelo menos, uma vez por dia.";
+const listTips = document.querySelectorAll(".tip")
+
+function changeTip(){
+    if (document.querySelector("#tip-1").classList.contains("selected")){
+        document.querySelector("#tip-1").classList.remove("selected");
+        document.querySelector("#tip-2").classList.add("selected");
+        document.querySelector("#tip-text").textContent = text2;
+    }
+    else if (document.querySelector("#tip-2").classList.contains("selected")){
+        document.querySelector("#tip-2").classList.remove("selected");
+        document.querySelector("#tip-3").classList.add("selected");
+        document.querySelector("#tip-text").textContent = text3;
+    }
+    else if (document.querySelector("#tip-3").classList.contains("selected")){
+        document.querySelector("#tip-3").classList.remove("selected");
+        document.querySelector("#tip-1").classList.add("selected");
+        document.querySelector("#tip-text").textContent = text1;
+    }
+}
+
+listTips.forEach(tip => {
+    tip.addEventListener("click", () => {
+        listTips.forEach(element => {
+            if(element.classList.contains("selected")){
+                element.classList.remove("selected");
+            }
+        });
+
+        tip.classList.add("selected");
+        if (document.querySelector("#tip-1").classList.contains("selected")){
+            document.querySelector("#tip-text").textContent = text1;
+        }
+        else if (document.querySelector("#tip-2").classList.contains("selected")){
+            document.querySelector("#tip-text").textContent = text2;
+        }
+        else if (document.querySelector("#tip-3").classList.contains("selected")){
+            document.querySelector("#tip-text").textContent = text3;
+        }
+    })
+});
+
+setInterval(function(){ 
+     changeTip();
+}, 3000);
